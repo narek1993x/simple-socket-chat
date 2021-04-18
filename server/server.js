cc07ba4416636f41d7e166e12e472d3892ef31cd
Narek Khachatryan <Narek_Khachatryan1@epam.com>
2021-04-18 19:47:05 +0400

    update express

--- server/server.js
+++ server/server.js
@@ -4,9 +4,7 @@ const socketIO = require("socket.io");
 
 const PORT = process.env.PORT || 3001;
 
-const server = express()
-  // .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const server = express().listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
 
